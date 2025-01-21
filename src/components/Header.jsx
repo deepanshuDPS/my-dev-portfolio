8ddafdc2983c6949622c0d06892d436d6c99ba3d
deepanshu-hh <deepanshu.a@hubhopper.com>
2025-01-21 14:29:14 +0530

    first version content done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -27,18 +27,20 @@ const Header = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  // return(<></>)
+
   // pb-4
   return (
     <nav class={`${headerBg} z-10`}>
       <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 ">
         <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
-          <img src="top_logo.png" class="h-16" alt="Flowbite Logo" />
+          <img src="top_logo.png" class="h-8 md:h-16" />
           <span class="self-center text-xl md:text-2xl font-semibold whitespace-nowrap ">Code to Craft</span>
         </a>
         <button
           onClick={toggleMenu}
           type="button"
-          class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 "
+          class="inline-flex items-center p-2 w-8 h-8 md:w-10 md:h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 "
           aria-controls="navbar-default"
           aria-expanded={isMenuOpen}>
           <span class="sr-only">Open main menu</span>
